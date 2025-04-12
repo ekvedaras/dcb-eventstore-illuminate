900e558dbf96fe6a1884fca20ab1ea7a3f6f6237
Ernestas Kvedaras <kvedarasernestas@gmail.com>
2025-04-12 14:14:22 +0300

    fix: cast to int

--- src/IlluminateEventStore.php
+++ src/IlluminateEventStore.php
@@ -162,7 +162,7 @@ final class IlluminateEventStore implements EventStore, Setupable
             try {
                 return $this->config->connection->transaction($statement);
             } catch (QueryException $e) {
-                if ($e->getCode() === 40001) {
+                if ((int) $e->getCode() === 40001) {
                     if ($retryAttempt >= $maxRetryAttempts) {
                         throw new RuntimeException(sprintf('Failed after %d retry attempts', $retryAttempt), 1686565685, $e);
                     }
